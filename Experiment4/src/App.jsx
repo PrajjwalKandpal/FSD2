5f8ad0fb29763ef31b14057010c94122d1e0aae3
PrajjwalKandpal <professionalworkpj@gmail.com>
2026-02-03 10:26:58 +0530

    "Second commit to exp4"

--- Experiment4/src/App.jsx
+++ Experiment4/src/App.jsx
@@ -6,7 +6,8 @@ import CounterReduxParent from "./components/redux/CounterGlobalReduxParent";
 import { CounterContext } from "./components/contextapi/CounterContextApi";
 
 export default function App() {
-  const [localCount, setLocalCount] = useState(0);
+  const [localCount1, setLocalCount1] = useState(0);
+  const [localCount2, setLocalCount2] = useState(0);
   const { count: contextCount } = useContext(CounterContext);
 
   return (
@@ -15,13 +16,21 @@ export default function App() {
       <h1><center>Name: Prajjwal Kandpal</center></h1>
       <h1><center>UID: 23BIS70052</center></h1>
       <h2><center>Experiment 4: State Management using Redux</center></h2>
-      {/* LOCAL STATE */}
-      <div className="card local">
-        <h2>2 : Local State Count: {localCount}</h2>
-        <div className="btn-row">
-          <LocalCounter count={localCount} setCount={setLocalCount} />
-        </div>
-      </div>
+     {/* LOCAL STATE */}
+<div className="card local">
+  <h2>1 : Local State Count: {localCount1}</h2>
+  <div className="btn-row">
+    <LocalCounter count={localCount1} setCount={setLocalCount1} />
+  </div>
+</div>
+
+<div className="card local">
+  <h2>2 : Local State Count: {localCount2}</h2>
+  <div className="btn-row">
+    <LocalCounter count={localCount2} setCount={setLocalCount2} />
+  </div>
+</div>
+
 
       {/* CONTEXT API */}
       <div className="card context">
