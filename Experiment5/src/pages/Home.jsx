f7ccdd674efb4c15931500773d729498a87f2e19
PrajjwalKandpal <professionalworkpj@gmail.com>
2026-02-04 11:51:21 +0530

    Initial Commit to Experiment 5 FSD2

---
+++ Experiment5/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="page">
+      <h2>Home Page</h2>
+      <p>
+        This page is NOT lazy loaded. It loads immediately when the app starts.
+      </p>
+    </div>
+  );
+}
