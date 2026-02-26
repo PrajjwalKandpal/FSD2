af18ae18b5f8368708dad4fb674c6613f77900ca
PrajjwalKandpal <professionalworkpj@gmail.com>
2026-02-26 14:25:58 +0530

    New commit

--- Experiment8/backend/rest-api-lab/app.py
+++ Experiment8/backend/rest-api-lab/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes.student_routes import student_bp
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -14,3 +15,7 @@ app = create_app()
 @app.route("/")
 def home():
     return {"message": "Backend Server is running"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
