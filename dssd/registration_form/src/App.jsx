c438155a870c138be9740ed79ea01eab9a5bc5e5
PrajjwalKandpal <professionalworkpj@gmail.com>
2026-03-09 10:36:56 +0530

    Initial Commit to MSTPRACT_23BIS70052_FSD2

---
+++ dssd/registration_form/src/App.jsx
@@ -0,0 +1,21 @@
+import Form from "./components/Form";
+
+export default function App() {
+  return (
+    <div>
+      {/* Page Title */}
+      <h2 style={{ textAlign: "center", marginTop: "20px" }}>
+        MST PRACTICAL : Handle Forms and Validations in Frontend (MUI)
+      </h2>
+
+      {/* Form */}
+      <Form />
+
+      {/* Name + UID */}
+      <div style={{ textAlign: "center", marginTop: "30px" }}>
+        <h3>Name : Prajjwal Kandpal</h3>
+        <h3>UID : 23BIS70052</h3>
+      </div>
+    </div>
+  );
+}
